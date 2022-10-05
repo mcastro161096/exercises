603cd989f0f743cae8bd0acc45d4e4b43c6fb3d1
Matheus Castro <MATHEUSCASTRO161096@gmail.com>
2022-10-04 23:27:33 -0300

    Multiple exports in one file.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {View} from 'react-native';
 
 import First from './components/First';
+import Oficial, {Comp1, Comp2} from './components/Mult';
 
 export default function App() {
   return (
     <View>
-      <First />
+      {/* <First /> */}
+      <Comp1 />
+      <Comp2 />
+      <Oficial />
     </View>
   );
 }
---
+++ src/components/Mult.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Comp = () => {
+  return <Text>Comp oficial</Text>;
+};
+export const Comp1 = () => {
+  return <Text>Comp1</Text>;
+};
+
+export const Comp2 = () => {
+  return <Text>Comp2</Text>;
+};
+
+export default Comp;
